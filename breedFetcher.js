7bf599987869703bd259ee7b724daedd13894cda
Fatima Altaf <fatima.altaf@gmail.com>
2019-10-24 20:07:56 +0000

    refactored breed fetcher and index code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,25 @@
-const url = "https://api.thecatapi.com/v1/breeds/search?q=" + process.argv[2];
 // const path = process.argv[3];
 
 const request = require('request');
-request(url, (error, response, body) => {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  
-  if (error) {
-    console.log("Request failed");
-  } else {
-    const data = JSON.parse(body);
-    if (data[0] === undefined) {
-      console.log("No cat exists");
+
+const fetchBreedDescription = function(breedName, callback) {
+ let url = "htps://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+  request(url, (error, response, body) => {
+    // console.log('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body); // Print the HTML for the Google homepage.
+    
+    if (error) {
+      callback(error, null);
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data[0] === undefined) {
+      callback(null, "No cat exists")
+      } else {
+        console.log(data[0].description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
